87f17b9acf311a53c0ca726b307960d76d5c960f
Sameer Yogi <sameeryog99@gmail.com>
2026-02-27 19:27:56 -0500

    Initial commit: Day 1 and Day 2

---
+++ Day_2/variables.py
@@ -0,0 +1,33 @@
+# Day 2: 30 Days of Python Programming
+
+first_name = "Sameer"
+last_name = "Yogi"
+full_name = "Sameer Yogi"
+country = "Nepal"
+city = "Kathmandu"
+age = 21
+year = 2004
+is_married = False
+is_true = False
+is_light_on = True
+school,city = "georgia state university", "atlanta"
+
+print(type(is_married))
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+print(total)
+
+diff = num_one - num_two
+print(diff)
+
+product = num_one * num_two
+print(product)
+
+division = num_one/num_two
+print(division)
+
