f73cd97a7c28b5ba3a0f905bdff2d3244eb889c8
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-04 12:27:08 -0500

    Added question1 of level 2

---
+++ Day_6/family.py
@@ -0,0 +1,6 @@
+#Unpack siblings and parents from family_members
+family_members = ('Sameer', 'Saurav', 'Samrat', 'Pragig', 'Sampada', 'Pranita', 'Tika', 'Dhan Nath')
+siblings = family_members[0:-2]
+print(siblings)
+parents = family_members[-2:]
+print(parents)
