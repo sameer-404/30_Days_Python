b02490b5ec5b2b194ffc9e43c46e4d207ae13b39
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-04 12:49:22 -0500

    Finished the both level of exercises

---
+++ Day_6/nordic.py
@@ -0,0 +1,16 @@
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+#Check if 'Estonia' is a nordic country
+result1 = "Estonia" in nordic_countries
+if result1 == True:
+  print("Estonia is a nordic country!")
+else:
+  print("Estonia is not a nordic country!")
+
+
+#Check if 'Iceland' is a nordic country
+result2 = "Iceland" in nordic_countries
+if result2 == True:
+  print("Iceland is a nordic country!")
+else:
+  print("Iceland is not a nordic country!")
