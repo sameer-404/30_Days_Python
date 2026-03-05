375a98bbd48ca651ed7693dbd65ccc3b61e6136f
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-05 01:44:36 -0500

    Finished practice

--- Day_7/practice.py
+++ Day_7/practice.py
@@ -115,3 +115,38 @@ print(intersection_of_both)"""
 dragon = {"d", "r", "a", "g", "o", "n"}
 inter= python&dragon
 print(inter)"""
+
+#Checking Subsets and super set:
+#A set can be a subset pr superset of other sets:
+#Subset: issubset()
+#Superset: isuperset()
+
+"""whole_numbers = {0,1,2,3,4,5,6,7,8,9,10}
+even_numbers = {0,2,4,6,8,10}
+result1 =whole_numbers.issubset(even_numbers) #False, cause its a super set
+print(result1)
+result2 = whole_numbers.issuperset(even_numbers)
+print(result2)"""
+
+#Checking the difference between two sets:
+#It returns the difference between two sets or using - symbol
+"""whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+even_numbers = {0, 2, 4, 6, 8, 10}
+diff = whole_numbers.difference(even_numbers)
+print(diff)
+
+python = {"p", "y", "t", "h", "o", "n"}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+python_diff_dragon = python.difference(dragon)
+print(python_diff_dragon)
+dragon_diff_python = dragon.difference(python)
+print(dragon_diff_python)"""
+
+#Disjoint Sets:
+#If two sets do not have a common item or items we call them disjoint sets.
+#We can check if two sets are joint or disjoint using isdisjoint() method
+"""even_numbers = {0,2,4,6,8}
+odd_numbers = {1,3,5,7,9}
+result = even_numbers.isdisjoint(odd_numbers)
+print(result)
+"""
