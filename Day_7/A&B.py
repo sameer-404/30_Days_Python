5e898d8cd2bf30140121484d8cb6a65c1f613463
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-05 02:10:37 -0500

    Finished level 2

--- Day_7/A&B.py
+++ Day_7/A&B.py
@@ -16,3 +16,19 @@ print(result)
 #Are A and B disjoint sets
 result2 = A.isdisjoint(B)
 print(result2)
+
+#Join A with B and B with A
+A_join_B = A.union(B)
+B_join_A = B.union(A)
+print(A_join_B)
+print(B_join_A)
+
+#What is the symmetric difference between A and B
+diff = A.symmetric_difference(B)
+print(diff)
+
+#Delete the sets completely 
+del A
+del B
+#print(A) will raise an error
+#print(B) will raise an error 
