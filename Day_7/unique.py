8e30489349ea617d8c8a2078c7041da88aa3019f
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-05 02:35:17 -0500

    Finished day 7

---
+++ Day_7/unique.py
@@ -0,0 +1,7 @@
+sentence = "I am a teacher and I love to inspire and teach people"
+splitted_list = sentence.split()
+print(splitted_list)
+splitted_set = set(splitted_list)
+unique_words = len(splitted_set)
+print(splitted_set)
+print(f"Number of unique words: {unique_words  }")
