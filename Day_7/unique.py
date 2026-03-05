b01fda499669cac1c1ee9352f0471d7a97b244d0
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-05 02:36:32 -0500

    Just adding the question

--- Day_7/unique.py
+++ Day_7/unique.py
@@ -1,3 +1,5 @@
+#I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
+
 sentence = "I am a teacher and I love to inspire and teach people"
 splitted_list = sentence.split()
 print(splitted_list)
