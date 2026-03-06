a37d118b10ef3d6f139471df8bc61246ca45b6a6
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-06 18:10:09 -0500

    Finished 2nd question

---
+++ Day_10/deiterate.py
@@ -0,0 +1,8 @@
+#Iterate 10 to 0 using for loop, do the same using while loop.
+for number in range(10, -1, -1):
+    print(number)
+
+number = 10
+while number >= 0:
+    print(number)
+    number -= 1
