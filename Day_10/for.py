66e6e74a5e3354a2ce9f6f76db5c1586f0d91a86
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-06 18:26:53 -0500

    Finished 6

---
+++ Day_10/for.py
@@ -0,0 +1,5 @@
+#Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+our_list = ["Python", "Numpy", "Pandas", "Django", "Flask"]
+
+for language in our_list:
+  print(language)
