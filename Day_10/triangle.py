8d4d76c27c7b21c51f02539dd601a8818f379216
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-06 18:18:31 -0500

    Finished 2

---
+++ Day_10/triangle.py
@@ -0,0 +1,13 @@
+#Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+"""
+#
+##
+###
+####
+#####
+######
+#######
+"""
+
+for i in range(1,8):
+  print("#" * i)
