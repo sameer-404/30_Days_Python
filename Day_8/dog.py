c42f38971034c05bae44153cc6f6c968618d6f62
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-06 11:05:08 -0500

    Finished 2 questions

---
+++ Day_8/dog.py
@@ -0,0 +1,10 @@
+#Create an empty dictionary called dog
+dog = dict()
+
+#Add name, color, breed, legs, age to the dog dictionary
+dog["name"] = "puppy"
+dog["color"] = "white"
+dog["breed"] = "Japnese Spitz"
+dog["legs"] = 4
+dog["age"] = 7
+print(dog)
