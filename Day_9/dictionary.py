cb2d674b20335105b6ef7f2db36ff7d59bdbc24e
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-06 14:21:39 -0500

    .

---
+++ Day_9/dictionary.py
@@ -0,0 +1,33 @@
+#Here we have a person dictionary. Feel free to modify it!
+
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+#Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
+
+if "skills" in person:
+    skills = person["skills"]
+    middle = len(skills) // 2
+    print(f"Middle skill: {skills[middle]}")
+
+
+#Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
+
+if "skills" in person:
+    skills = person["skills"]
+    if "Python" in skills:
+        print("Python was there in skills!")
+    else:
+        print("There was no skill like Python!")
+
+#If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
