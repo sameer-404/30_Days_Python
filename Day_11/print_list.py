087fa09c0029e239696f1276a884c12013ae49c3
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-08 22:36:39 -0400

    Finished another

---
+++ Day_11/print_list.py
@@ -0,0 +1,10 @@
+#Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
+
+def print_list(my_list):
+  
+  for each in my_list:
+    print(each)
+
+print_list(["I", "am", "Sameer", "Yogi"])
+
+
