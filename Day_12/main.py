47fe789047e8de277b645f862d147c119e085719
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-10 00:36:38 -0400

    Finished lectures

---
+++ Day_12/main.py
@@ -0,0 +1,8 @@
+"""import mymodule
+
+print(mymodule.generate_full_name("Sameer", "Yogi"))
+"""
+# main.py file
+from mymodule import generate_full_name as full_name, sum_two_nums as two_sums
+print(full_name('Sameer','Yogi'))
+print(two_sums(1,9))
---
+++ Day_12/mymodule.py
@@ -0,0 +1,6 @@
+def generate_full_name(firstname, lastname):
+    return firstname + ' ' + lastname
+
+def sum_two_nums(a,b):
+    sum = a + b
+    return sum
