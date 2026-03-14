9527e99575a736c242256c28e828e9f2b0ddd4b3
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-13 23:56:00 -0400

    .

--- Day_14/practice.py
+++ Day_14/practice.py
@@ -9,3 +9,8 @@ A function can be returned as a result of another function
 A function can be modified
 A function can be assigned to a variable
 """
+
+#In this section, we will cover:
+#Handling functions as parameters
+#Returning functions as return value from another functions
+#Using Python closures and decorators
