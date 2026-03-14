9382f31150af032b562520d7ee5bacd27fa9d138
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-13 23:53:38 -0400

    Started Day 14

---
+++ Day_14/practice.py
@@ -0,0 +1,11 @@
+#Dqy - 14: Higher Order Function!
+
+#Higher Order Functions
+
+#In Python functions are treated as first class citizens, allowing you to perform the following operations on functions:
+"""
+A function can take one or more functions as parameters
+A function can be returned as a result of another function
+A function can be modified
+A function can be assigned to a variable
+"""
