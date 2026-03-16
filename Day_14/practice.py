5c08a472b8c5cc97d49b176a607e79d45fd3299d
Sameer Yogi <sameeryoogi99@gmail.com>
2026-03-15 23:52:19 -0400

    Finished another

--- Day_14/practice.py
+++ Day_14/practice.py
@@ -16,4 +16,12 @@ A function can be assigned to a variable
 #Using Python closures and decorators
 
 
-#Something
+#Function as a Parameter
+def sum_numbers(nums):  # normal function
+    return sum(nums)    # a sad function abusing the built-in sum function :<
+
+def higher_order_function(f, lst):  # function as a parameter
+    summation = f(lst)
+    return summation
+result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
+print(result)       # 15
